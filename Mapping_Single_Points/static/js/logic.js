f4c00d435a49de9c33ad91d7a85b5ca9b7a4b80a
MattK <MattK1454@users.noreply.github.com>
2020-12-18 20:23:47 -0600

    City marker added

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,6 +7,8 @@ let map = L.map('mapid', {
     zoom: 4
 });
 
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -14,5 +16,9 @@ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStree
     id: 'mapbox/streets-v11',
     accessToken: API_KEY
 });
+
+// Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
