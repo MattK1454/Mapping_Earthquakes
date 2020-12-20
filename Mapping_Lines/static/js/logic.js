57dab230b42f6a81031a67859325e06f3ad84008
MattK <MattK1454@users.noreply.github.com>
2020-12-20 07:32:02 -0600

    line map added

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,7 +3,20 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 
-let map = L.map('mapid').setView([31.3038, -96.2528], 4);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+    color: "yellow"
+}).addTo(map);
 
 // Create the map object with a center and zoom level.
 // let map = L.map('mapid', {
@@ -15,25 +28,25 @@ let map = L.map('mapid').setView([31.3038, -96.2528], 4);
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
     accessToken: API_KEY
 });
 
 // Get data from cities.js
-let cityData = cities;
+// let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city);
-    L.circleMarker(city.location, {
-        radius: city.population/200000,
-        weight: 4,
-        color: "orange",
-        opacity: 0.5
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+// cityData.forEach(function(city) {
+//     console.log(city);
+//     L.circleMarker(city.location, {
+//         radius: city.population/200000,
+//         weight: 4,
+//         color: "orange",
+//         opacity: 0.5
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
